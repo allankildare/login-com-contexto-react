72ba59c263f416610255a07c3a2320a06c81722d
allankildare <allankildare27@gmail.com>
2022-01-11 00:09:43 -0300

    feat: array de usuarios falsos

---
+++ src/components/UserContext/users.js
@@ -0,0 +1,8 @@
+export const users = [
+    { username: 'allan_kildare', password: 'fake-pass-allan' },
+    { username: 'beatriz_silva', password: 'fake-pass-bia' },
+    { username: 'carina_soares', password: 'fake-pass-carina' },
+    { username: 'daniel_carvalho', password: 'fake-pass-dani' },
+    { username: 'eduarda_esteves', password: 'fake-pass-duda' },
+    { username: 'fabricio_costa', password: 'fake-pass-fabricio' }
+]
